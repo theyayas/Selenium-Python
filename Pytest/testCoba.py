ce5a9eaff5277b1c4526fc1f8856499212bf2498
Muhammad Yasin <yasingammarkanari@gmail.com>
2023-07-20 20:27:06 +0800

    Pytest : nama file harus mengandung kata test

---
+++ Pytest/testCoba.py
@@ -0,0 +1,39 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.common.exceptions import TimeoutException
+import time
+
+driver = webdriver.Chrome()
+driver.minimize_window()
+
+def test_bukaWeb():
+    for i in range(2):
+        driver.get("https://tees.co.id/")
+        driver.implicitly_wait(2)
+
+        '''
+        menggunakan WebDriverWait, expected_condition, dan TimeoutException untuk menunggu apakah pup up muncul atau tidak
+        karena pop up seperti ini kadang muncul, kadang tidak muncul
+        '''
+        try:
+            WebDriverWait(driver, 5).until(EC.visibility_of_element_located((By.XPATH, "/html/body/div[1]/div/div/div/div/div[2]/div")))
+            print("popup muncul")
+            driver.find_element(By.CLASS_NAME, "btn-modal-close").click()
+            print("popup ditutup")
+        except  TimeoutException:
+            print("pop up tidak muncul")
+            pass
+
+    #malah coba negatif case login yahaha
+    driver.find_element(By.XPATH, "//*[@id='navbar']/div[3]/submenu-navbar/div/div/ul[2]/li[3]").click()
+    driver.find_element(By.ID, "username").send_keys("yahahaha")
+    driver.find_element(By.ID, "password").send_keys("yohohoho")
+    driver.find_element(By.XPATH, "/html/body/div[3]/div/div/login-view/div/div[2]/div/div/form/div[3]/button").click()
+    validasi = driver.find_element(By.XPATH, "/html/body/div[3]/div/div/login-view/div/div[2]/div/div/form/p").text
+    print("Tidak dapat login syng")
+    time.sleep(3)
+
+    assert validasi == "Username atau password salah."
+
--- Pytest/test_selenium.py
+++ Pytest/test_selenium.py
@@ -1,10 +1,16 @@
 from selenium import webdriver
 
 driver = webdriver.Chrome()
-driver.maximize_window()
+driver.minimize_window()
 
 def test_bukaWeb():
     driver.get("https://demoqa.com/")
     title = driver.title                # akan digunakan sebagai validasi apakah web yang terbuka sudah benar
 
-    assert title == "DEMOQA"             # validasi web yang terbuka adalah web DEMOQA iya
+    assert title == "DEMOQA"            # validasi web yang terbuka adalah web DEMOQA
+
+def test_bukaWebTheInternet():
+    driver.get("https://the-internet.herokuapp.com/")
+    title = driver.title                # akan digunakan sebagai validasi apakah web yang terbuka sudah benar
+
+    assert title == "The Internet"      # validasi web yang terbuka adalah web The Internet
